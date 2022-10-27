fc79f080a3ec918774a8cdfbe960b193e9a65a36
Victoria Mertens <victoria.mertens44@gmail.com>
2022-10-27 11:54:20 -0500

    Complete comment plan

--- script.js
+++ script.js
@@ -15,6 +15,23 @@ function onReady() {
   //listen for star-fire click, run starFireAttack
 }
 
+//Going to want global variables for the AP and HP
+
+//Attack function will generally...
+//set the attack HP variable for that specific attack
+//set the attack AP variable for that specific attack
+//update the state of global AP and HP variabes
+//included in this is running a check if it is neg, be 0
+//run a function to render the state onto the DOM
+
+//function render will have two steps
+//1. update the AP HP variables on the DOM
+//2. run a seperate function checkVictoryStatus to check if 0
+
+//function checkVictoryStatus will be conditional to test 0
+//if AP 0, run renderUserWon
+//if HP 0, run renderFungusWon
+
 // 🧠 Remember
 // - Handle events that ->
 // - Updates state which is ->
