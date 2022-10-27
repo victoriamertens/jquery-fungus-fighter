3bc5d1b315922763e1d074f58c80be7b92420087
Victoria Mertens <victoria.mertens44@gmail.com>
2022-10-27 13:24:45 -0500

    Started updating the DOM

--- script.js
+++ script.js
@@ -10,24 +10,56 @@ $(document).ready(onReady);
 
 function onReady() {
   //listen for arcane-scepter click, run arcaneSceptreAttack
+  $('.arcane-sceptre').on('click', arcaneSceptreAttack);
   //listen for entangle click, run entangleAttack
+  $('.entangle').on('click', entangleAttack);
   //listen for dragon-blade click, run dragonBladeAttack
+  $('.dragon-blade').on('click', dragonBladeAttack);
   //listen for star-fire click, run starFireAttack
+  $('.star-fire').on('click', starFireAttack);
 }
 
 //Going to want global variables for the AP and HP
+let attackPoints = 100;
+let healthPoints = 100;
 
-//Attack function will generally...
-//set the attack HP variable for that specific attack
-//set the attack AP variable for that specific attack
-//update the state of global AP and HP variabes
-//included in this is running a check if it is neg, be 0
-//run a function to render the state onto the DOM
+function arcaneSceptreAttack() {
+  console.log('in arcane sceptre');
+  //Attack function will generally...
+  //set the attack HP variable for that specific attack
+  const healthDamage = 14;
+  //set the attack AP variable for that specific attack
+  const attackCost = 12;
+  //update the state of global AP and HP variabes
+  attackPoints -= attackCost;
+  healthPoints -= healthDamage;
+  //included in this is running a check if it is neg, be 0
+  zeroFloorCheck();
+  //run a function to render the state onto the DOM
+  renderCurrentPoints();
+}
 
-//function render will have two steps
-//1. update the AP HP variables on the DOM
-//2. run a seperate function checkVictoryStatus to check if 0
+//function zeroFloorCheck will double check negative
+function zeroFloorCheck() {
+  console.log('in zeroFloorCheck');
+  if (attackPoints < 0) {
+    attackPoints = 0;
+  }
+  if (healthPoints < 0) {
+    healthPoints = 0;
+  }
+}
 
+function renderCurrentPoints() {
+  console.log('in the renderCurrentPoints function');
+  console.log(healthPoints);
+  //function renderCurrentPoints will have two steps
+  //1. update the AP HP variables on the DOM
+  $('.hp-text').html(`<div class="hp-text">` + healthPoints + ` HP </div>`);
+  $('.ap-text').html(`<div class="ap-text">` + attackPoints + ` AP </div>`);
+  //2. run a seperate function checkVictoryStatus to check if 0
+  checkVictoryStatus();
+}
 //function checkVictoryStatus will be conditional to test 0
 //if AP 0, run renderUserWon
 //if HP 0, run renderFungusWon
